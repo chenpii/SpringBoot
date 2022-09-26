a6d32225a745fce1bab18ac584539e8d5bcf1089
chenh <641760512@qq.com>
2022-09-26 22:47:17 +0800

    修改端口和banner

---
+++ springboot-01-helloworld/src/main/java/com/chenpii/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.chenpii.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author chenpi
+ * @create 2022-09-26 22:36
+ */
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "hello";
+
+    }
+}
